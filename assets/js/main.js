876936267f880b59000526c466000755771f84b1
RegiTheWitch <fakhraaaaa@mail.ru>
2023-09-07 12:32:51 +0300

    123

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// alert('Практическая работа №2');
+
+// console.log('123');
+
+let name = prompt ('Введите название города');
+let year = prompt ('Введите год образования города');
+let population = prompt ('Введите население города');
+year=Number(year);
+let year_1= 2023- year;
+alert ('Городу ' + name + ' исполнилось ' + year_1+ ' лет с момента его образования. Население - ' + population + ' человек');
+
+
